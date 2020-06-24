fa22c4696bc44bcbc4ae75dccdad1a1c3d9cf2ae
Sundaram Dubey <56407566+maze-runnar@users.noreply.github.com>
2020-06-24 21:11:50 +0530

    Update data.js

--- src/mock/data.js
+++ src/mock/data.js
@@ -29,9 +29,9 @@ export const projectsData = [
   {
     id: uuidv1(),
     img: 'scholar.jpg',
-    title: 'Google-Scholar',
+    title: 'MyScholar',
     info: 'Google scholar is a metric computation system for researchers with a Google Scholar profile. Google Scholar provides researchers with stats such as the number of publications, citations, h-index and i10 index. But, these metrics are flawed. my Google Scholar extracts some basic information form Google Scholar and computes better metrics, and displays them on another website. So, researchers can now see better, effective metrics with a single click.',
-    info2: '',
+    info2: 'And Scholarly implementation with website to get raw data with fast speed.',
     url:'https://friendly-poincare-d05cd9.netlify.app',
     repo: 'https://github.com/maze-runnar/Google-Scholar', // if no repo, the button will not show up
   },
